fdbceedb0a919b1c9e69866c9c09e849993fb195
prem <naraynpremnl1304@gmail.com>
2026-07-21 20:11:31 +0530

    Updated  the import in FarmEntry.jsx

---
+++ src/Components/FarmCropComponent/FarmEntry.jsx
@@ -0,0 +1,4 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import "../../DisplayView.css";
+import { registerNewUser } from "../../Services/LoginService";
