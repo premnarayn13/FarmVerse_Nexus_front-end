5350f22908dd5d2379f62812639311db2f821d11
prem <naraynpremnl1304@gmail.com>
2026-07-21 20:36:43 +0530

    Updated the app.js by adding route to the FarmEntry

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter,Routes,Route } from 'react-router-dom';
 import LoginPage from './Components/LoginComponents/LoginPage';
 import RegisterUser from './Components/LoginComponents/RegisterUser';
 import FarmerMenu from './Components/LoginComponents/FarmerMenu';
+import FarmEntry from './Components/FarmCropComponent/FarmEntry';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
           <Route path="/" element={<LoginPage />} />
           <Route path="/register" element={<RegisterUser />} />
           <Route path="/farmer-menu" element={<FarmerMenu />} />
+          <Route path="/farm-add" element={<FarmEntry />} />
         </Routes>
       </BrowserRouter>
     </div>
