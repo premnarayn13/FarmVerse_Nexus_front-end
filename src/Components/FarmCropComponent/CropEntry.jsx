31ffa869cd33274ab63de72868348c2de2058423
prem <naraynpremnl1304@gmail.com>
2026-07-22 21:31:39 +0530

    Updating code for having const of farm details

---
+++ src/Components/FarmCropComponent/CropEntry.jsx
@@ -0,0 +1,25 @@
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { addCrop, generateCropId } from "../../Services/CropService";
+import "../../DisplayView.css";
+
+const CropEntry = () => {
+  let navigate = useNavigate();
+  const [errors, setErrors] = useState({});
+  const [crop, setCrop] = useState({
+    cropId: "",
+    farmId: 0,
+    username: "",
+    cropName: "",
+    cropArea: 0.0,
+    sownMonthYear: "",
+    harvestMonthYear: "",
+    yield: 0.0,
+  });
+  
+  return (
+    
+  );
+};
+
+export default CropEntry;
