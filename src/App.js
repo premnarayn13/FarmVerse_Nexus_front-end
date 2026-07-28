97e040bcb207ab3a836dc5f30c6e21bcd35d0a17
prem <naraynpremnl1304@gmail.com>
2026-07-28 23:36:49 +0530

    Altered App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import './App.css';
-import { BrowserRouter,Routes,Route } from 'react-router-dom';
-import LoginPage from './Components/LoginComponents/LoginPage';
-import RegisterUser from './Components/LoginComponents/RegisterUser';
-import FarmerMenu from './Components/LoginComponents/FarmerMenu';
-import FarmEntry from './Components/FarmCropComponent/FarmEntry';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LoginPage from "./Components/LoginComponent/LoginPage";
+import RegisterUser from "./Components/LoginComponent/RegisterUser";
+import FarmerMenu from "./Components/LoginComponent/FarmerMenu";
+import FarmEntry from "./Components/FarmCropComponent/FarmEntry";
+import CropEntry from "./Components/FarmCropComponent/CropEntry";
+import FarmList from "./Components/FarmCropComponent/FarmList";
+import CropList from "./Components/FarmCropComponent/CropList";
 
 function App() {
   return (
@@ -14,6 +19,9 @@ function App() {
           <Route path="/register" element={<RegisterUser />} />
           <Route path="/farmer-menu" element={<FarmerMenu />} />
           <Route path="/farm-add" element={<FarmEntry />} />
+          <Route path="/crop-add" element={<CropEntry />} />
+          <Route path="/farm-list" element={<FarmList />} />
+          <Route path="/crop-list" element={<CropList />} />
         </Routes>
       </BrowserRouter>
     </div>
