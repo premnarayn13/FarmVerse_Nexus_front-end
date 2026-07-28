e44756d4061a09522dd5d674e4756937eb212958
prem <naraynpremnl1304@gmail.com>
2026-07-28 23:11:36 +0530

    Fixed FarmService and CropEntry and creaded new Component file named FarmList.jsx

--- src/Components/FarmCropComponent/CropEntry.jsx
+++ src/Components/FarmCropComponent/CropEntry.jsx
@@ -4,8 +4,10 @@ import { addCrop, generateCropId } from "../../Services/CropService";
 import "../../DisplayView.css";
 
 const CropEntry = () => {
+  
   let navigate = useNavigate();
   const [errors, setErrors] = useState({});
+  
   const [crop, setCrop] = useState({
     cropId: "",
     farmId: 0,
@@ -16,6 +18,7 @@ const CropEntry = () => {
     harvestMonthYear: "",
     yield: 0.0,
   });
+
   const [flag, setFlag] = useState(false);
   const [newId, setNewId] = useState("");
   const [newId, setNewId] = useState(0);
@@ -23,12 +26,13 @@ const CropEntry = () => {
   const setCropId = () => {
     generateCropId().then((response) => {
       setNewId(response.data);
-    });
-  };
-  useEffect(() => {
-    setCropId();
-    setFlag(false);
-  }, []);
+      });
+    };
+
+    useEffect(() => {
+        setCropId();
+        setFlag(false);
+    }, []);
 
     const onChangeHandler = (event) => {
     event.persist();
@@ -36,7 +40,8 @@ const CropEntry = () => {
     const name = event.target.name;
     const value = event.target.value;
     setCrop((values) => ({ ...values, [name]: value }));
-  };
+    };
+
 
     const saveCrop = (event) => {
     event.preventDefault();
@@ -44,17 +49,20 @@ const CropEntry = () => {
     addCrop(crop).then((response) => {
       setFlag(true);
     });
-  };
+    };
+
 
     const clearAll = () => {
     crop.cropName = "";
     crop.area = 0.0;
-  };
+    };
+
 
     if (!toString(crop.cropName).trim()) {
       tempErrors.cropName = "Crop name is required";
       isValid = false;
     }
+
 
     setErrors(tempErrors);
     if (isValid) {
@@ -68,75 +76,75 @@ const CropEntry = () => {
 
 
   return (
-<div>
-      <br />
-      <div className="container">
-        <div className="row">
-          <div className="card col-md-2 offset-md-3 offset-md-3">
-            <div className="login-box">
-              <h2 className="text-center">New Crop Entry</h2>
-              <br />
-              <form>
-                <div className="form-group">
-                  <label>Crop Id: </label>
-                  <input
-                    placeholder="Crop Id"
-                    name="cropId"
-                    className="form-control"
-                    value={newId}
-                  />
-                </div>
-                <div className="form-group">
-                  <label> Crop Name: </label>
-                  <input
-                    placeholder="Crop Name"
-                    name="cropName"
-                    className="form-control"
-                    value={crop.cropName}
-                    onChange={onChangeHandler}
-                  />
-                  {errors.cropName && (
-                    <p style={{ color: "red" }}>{errors.cropName}</p>
-                  )}
-                </div>
+    <div>
+        <br/>
+        <div className="container">
+            <div className="row">
+            <div className="card col-md-2 offset-md-3 offset-md-3">
+                <div className="login-box">
+                <h2 className="text-center">New Crop Entry</h2>
+                <br />
+                <form>
+                    <div className="form-group">
+                    <label>Crop Id: </label>
+                    <input
+                        placeholder="Crop Id"
+                        name="cropId"
+                        className="form-control"
+                        value={newId}
+                    />
+                    </div>
+                    <div className="form-group">
+                    <label> Crop Name: </label>
+                    <input
+                        placeholder="Crop Name"
+                        name="cropName"
+                        className="form-control"
+                        value={crop.cropName}
+                        onChange={onChangeHandler}
+                    />
+                    {errors.cropName && (
+                        <p style={{ color: "red" }}>{errors.cropName}</p>
+                    )}
+                    </div>
 
-                <div className="form-group">
-                  <label> Crop Area: </label>
-                  <input
-                    placeholder="Crop Area"
-                    name="area"
-                    className="form-control"
-                    value={crop.area}
-                    onChange={onChangeHandler}
-                  />
-                  {errors.area && <p style={{ color: "red" }}>{errors.area}</p>}
-                </div>
+                    <div className="form-group">
+                    <label> Crop Area: </label>
+                    <input
+                        placeholder="Crop Area"
+                        name="area"
+                        className="form-control"
+                        value={crop.area}
+                        onChange={onChangeHandler}
+                    />
+                    {errors.area && <p style={{ color: "red" }}>{errors.area}</p>}
+                    </div>
 
-                <div className="form-group">
-                  <button
-                    className="btn btn-success"
-                    onClick={handleValidation}
-                  >
-                    Save
-                  </button>
-                  &nbsp;&nbsp;
-                  <button className="btn btn-secondary" onClick={clearAll}>
-                    Reset
-                  </button>
-                  &nbsp;&nbsp;
-                  <button className="btn btn-warning" onClick={returnBack}>
-                    Return Back
-                  </button>
+                    <div className="form-group">
+                    <button
+                        className="btn btn-success"
+                        onClick={handleValidation}
+                    >
+                        Save
+                    </button>
+                    &nbsp;&nbsp;
+                    <button className="btn btn-secondary" onClick={clearAll}>
+                        Reset
+                    </button>
+                    &nbsp;&nbsp;
+                    <button className="btn btn-warning" onClick={returnBack}>
+                        Return Back
+                    </button>
+                    </div>
+                </form>
+                <br />
+                <div>
+                    {flag && <p style={{ color: "blue" }}>New Crop Added </p>}
                 </div>
-              </form>
-              <br />
-              <div>
-                {flag && <p style={{ color: "blue" }}>New Crop Added </p>}
-              </div>
+                </div>
             </div>
-          </div>
+            </div>
         </div>
-      </div>
     </div>
   );
 };
--- src/Services/FarmService.jsx
+++ src/Services/FarmService.jsx
@@ -39,3 +39,4 @@ export const deleteFarm = (id) => {
         withCredentials: true
     });
 };
+
