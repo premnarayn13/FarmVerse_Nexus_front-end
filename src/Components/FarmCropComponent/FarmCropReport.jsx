daebf7052a03ab8ebad5c5106d3b5cbc79d254e4
prem <naraynpremnl1304@gmail.com>
2026-07-29 20:34:58 +0530

    added useNavigate function in FarmCropReport.jsx

--- src/Components/FarmCropComponent/FarmCropReport.jsx
+++ src/Components/FarmCropComponent/FarmCropReport.jsx
@@ -3,3 +3,17 @@ import {useNavigate,useParams,} from 'react-router-dom';
 import {getExpectedYield} from  "../../Services/AIService";
 import '../../DisplayView.css';
 
+let navigate=useNavigate();
+    let param=useParams();
+    const [farmCrop,setFarmCrop]=useState({
+        farmId:0,
+        farmName:"",
+         soil:"",
+        cropId:"",
+        cropName:"",
+        cropArea:0.0,
+        sownMonthYear:"",
+        harvestMonthYear:"",
+        yield:0.0,
+        comments:""
+    });
