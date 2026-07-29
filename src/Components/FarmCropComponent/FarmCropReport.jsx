91f4e5795101410e66b7bc6ecb470c523b89dee6
prem <naraynpremnl1304@gmail.com>
2026-07-29 20:34:07 +0530

    Added code in FromCropReport.jsx

---
+++ src/Components/FarmCropComponent/FarmCropReport.jsx
@@ -0,0 +1,5 @@
+import React,{useState,useEffect} from 'react';
+import {useNavigate,useParams,} from 'react-router-dom';
+import {getExpectedYield} from  "../../Services/AIService";
+import '../../DisplayView.css';
+
